6c2752ba50838a7c34258cc6d3f0e580fcda4bd9
metodi94 <m@metodi.es>
2024-05-26 18:29:22 +0200

    prueba desplegar

--- src/main/java/es/tecnohackspro/demo/HolaMundoController.java
+++ src/main/java/es/tecnohackspro/demo/HolaMundoController.java
@@ -8,7 +8,7 @@ public class HolaMundoController {
 
     @GetMapping("/hola")
     public String holaMundo(){
-        return "Hola metodi";
+        return "Eres un jodido grande hijo de puta";
 
     }
 }
